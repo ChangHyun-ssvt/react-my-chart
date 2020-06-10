e481f682de8915f8de923f1af4e870ee797b4ccd
hot9998 <hot9998@naver.com>
2020-06-10 20:28:37 +0900

    프로젝트 시작

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import cheerio from "cheerio";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getChart = async () => {
+  try {
+    return await axios.get(
+      "https://cors-anywhere.herokuapp.com/https://www.melon.com/chart/index.htm"
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getHtml = () => {
+  getChart().then((html) => {
+    let titleList = [];
+    const $ = cheerio.load(html.data);
+    const bodyList = $("div.rank01").children("span");
+
+    bodyList.each((i, elem) => {
+      console.log($(this).find("a"));
+      titleList[i] = {
+        title: $(this).find("a").text(),
+      };
+      // console.log(titleList[i]);
+    });
+    return titleList;
+  });
+};
+
+class App extends Component {
+  componentDidMount() {
+    getHtml();
+  }
+  render() {
+    return <div className="App">Hello World!</div>;
+  }
 }
 
 export default App;
