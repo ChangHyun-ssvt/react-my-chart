e087659c74cef5f6cc8830e44fdb3f5aa1392c79
hot9998 <hot9998@naver.com>
2020-06-26 17:30:15 +0900

    Remove Redux

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-
-export default createStore((state, action) => {
-  if (state === undefined) {
-    return { melonChart: [] };
-  }
-  return state;
-}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
