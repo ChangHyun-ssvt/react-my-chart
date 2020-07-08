f283d5da32abcece3d5511829992787956aca0ea
ChangHyunKim <hot9998@naver.com>
2020-07-08 10:48:19 +0900

    Add docker-compose

--- client/src/Components/User/Register.tsx
+++ client/src/Components/User/Register.tsx
@@ -5,6 +5,7 @@ function Register() {
   const [form, setForm] = useState({
     userid: "",
     password: "",
+    passwordCheck: "",
     username: "",
     email: "",
   });
@@ -22,10 +23,39 @@ function Register() {
     });
   };
   const handleRegister = async () => {
+    if (form.userid === "") {
+      alert("아이디를 입력해주세요");
+      return false;
+    }
+    if (form.password === "") {
+      alert("패스워드를 입력해주세요");
+      return false;
+    }
+    if (form.passwordCheck === "") {
+      alert("패스워드 확인란을 입력해주세요");
+      return false;
+    }
+    if (form.username === "") {
+      alert("이름을 입력해주세요");
+      return false;
+    }
+    if (form.email === "") {
+      alert("이메일을 입력해주세요");
+      return false;
+    }
+    if (form.password != form.passwordCheck) {
+      alert("패스워드를 확인해주세요");
+      return false;
+    }
+    postRegister();
+  };
+
+  const postRegister = async () => {
     await axios
-      .post("/api/user/register", form)
+      .post("/api/user/register", { ...form })
       .then((res: AxiosResponse<any>) => {
         alert(res.data);
+        window.location.href = "/";
       });
   };
 
